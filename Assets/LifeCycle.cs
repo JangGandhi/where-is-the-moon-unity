04dcaeeadabbc2487608f8f8dbdbd6d16e76b614
JangGandhi <playhunjin@naver.com>
2026-07-09 23:16:31 +0900

    study: 유니티 기초 강좌 B8

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -1,56 +1,44 @@
-using System.Buffers.Text;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class LifeCycle : MonoBehaviour
 {
-    void Start()
-    {
-        // Vector3 vec = new Vector3(0, 0, 0); // 주문서
-        // transform.Translate(vec); // 주방에 주문 넣기
-    }
+    Vector3 target = new Vector3(2, 0.5f, 0);
+    Vector3 velo = Vector3.up;
     void Update()
     {
-        // if (Input.GetButton("Horizontal"))
-        // {
-        //     // Debug.Log("Horizon은 수평선, 왼쪽과 오른쪽을 의미합니다.
-        //     // Debug.Log($"출력 값은 버튼을 누른 시간과 비례합니다: {Input.GetAxis("Horizontal")}");
-        //     // Debug.Log($"출력 값은 버튼을 누른 시간과 상관없이 무조건 1을 가집니다: {Input.GetAxisRaw("Horizontal")}");
-        //     // 왼쪽 버튼과 오른쪽 버튼을 동시에 누를 경우 출력 값은 0입니다.
-        //     // 즉, GetAxix는 부드러운 이동, GetAxixRow는 즉시 이동이라고 할 수 있습니다.
-        //     // Axis는 축, Raw는 가공되지 않은 날것의 신호를 의미합니다.            
+        // MoveTowards
+        // MoveTowards는 매개변수로 현재 위치, 목표 위치, 속도를 받습니다.
+        // 단순 등속 이동으로, 매 프레임마다 일정한 속도를 유지하며 목적지까지 이동합니다.
+        // transform.position = Vector3.MoveTowards(transform.position, target, 0.01f);
 
-        //     switch (Input.GetAxisRaw("Horizontal"))
-        //     {
-        //         case -1:
-        //             Debug.Log("왼쪽으로 이동 중...");
-        //             break;
-        //         case 1:
-        //             Debug.Log("오른쪽으로 이동 중...");
-        //             break;
-        //         default:
-        //             Debug.Log("Horizontal Return 0");
-        //             break;
-        //     }
-        // }
-        // if (Input.GetButton("Vertical"))
-        // {
-        //     switch (Input.GetAxisRaw("Vertical"))
-        //     {
-        //         case -1:
-        //             Debug.Log("아래쪽으로 이동 중...");
-        //             break;
-        //         case 1:
-        //             Debug.Log("위쪽으로 이동 중...");
-        //             break;
-        //         default:
-        //             Debug.Log("Vertical Return 0");
-        //             break;
-        //     }
-        // }
+        // SmoothDamp
+        // SmoothDamp는 매개변수로 현재 위치, 목표 위치, 현재 속도(참조 Vector), 예상 도착 시간을 받으며
+        // 감속 이동으로, 목적지가 멀면 서서히 가속하고 가까우면 서서히 감속합니다.
+        // transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 10.0f);
 
-        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal") / 10.0f, Input.GetAxisRaw("Vertical")/10.0f, 0);
-        transform.Translate(vec);
+        // Lerp
+        // Lerp는 매개변수로 현재 위치, 목표 위치, 비율을 받습니다.
+        // 선형 보간 이동으로, 매 프레임마다 남은 거리의 일정 비율만큼 이동하여 빠른 속도로 출발하며 목적지가 가까워질 수록 느려집니다.
+        // transform.position = Vector3.Lerp(transform.position, target, 0.01f);
+
+        // Slerp
+        // Slerp는 매개변수로 현재 위치, 목표 위치, 비율을 받습니다.
+        // 구면 선형 보간 이동으로 속도는 Lerp와 동일하게 작동하지만, 원점(0, 0, 0)을 중심축 삼아 현재 위치에서 목적지까지 부드러운 호를 그리며 회전하듯이 이동합니다.
+        // 원점에서 가까우면 가파르게, 원점에서 멀면 완만하게
+        // transform.position = Vector3.Slerp(transform.position, target, 0.01f);
+
+        // deltaTime
+        // 이전 프레임 완료까지 걸린 시간
+        // 프레임이 낮을 경우 deltaTime은 높은 값을, 프레임이 높을 수록 deltaTime은 낮은 값을 가집니다.
+        // 프레임이 높으면 낚시가 쉬워지고 프레임이 낮으면 낚시가 어려워지는 불상사를 막기 위해(프레임률 차이로 생기는 계산 횟수를 보정하기 위해) 사용합니다.
+        // Translate : 벡터에 곱하기
+        // transform.Translate(vec * Time.deltaTime);
+        // Vector 함수 : 시간 매개변수에 곱하기
+        // transform.position = Vector3.Lerp(transform.position, target, 0.01f * Time.deltaTime);
+        // 프레임 독립인 SmoothDamp를 제외하면 대부분 프레임에 비례하여 계산 횟수가 늘어나기에 프레임에 영향을 받는 이동일 경우 deltaTime을 반드시 곱해야 합니다.
+        // deltaTime은 낮은 값이므로 별도의 speed로 속도를 한 번 더 보정해주는 게 일반적입니다.
+        float speed = 60.0f;
+        transform.position = Vector3.Lerp(transform.position, target, 0.01f * Time.deltaTime * speed);
+        // 초당 60 프레임을 유지하고 있다면 deltaTime은 1/60입니다.
     }
 }
